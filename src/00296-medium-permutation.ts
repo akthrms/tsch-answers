3b2ecdb9ca6a9a33f3cdda131ad83c043b00654e
akthrms <akt.hrms@gmail.com>
2022-04-23 14:16:12 +0900

    update

---
+++ src/00296-medium-permutation.ts
@@ -0,0 +1,34 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type Permutation<T, K = T> = [T] extends [never]
+  ? []
+  : T extends T
+  ? [T, ...Permutation<Exclude<K, T>>]
+  : never;
+
+type cases = [
+  Expect<Equal<Permutation<"A">, ["A"]>>,
+  Expect<
+    Equal<
+      Permutation<"A" | "B" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
+  Expect<
+    Equal<
+      Permutation<"B" | "A" | "C">,
+      | ["A", "B", "C"]
+      | ["A", "C", "B"]
+      | ["B", "A", "C"]
+      | ["B", "C", "A"]
+      | ["C", "A", "B"]
+      | ["C", "B", "A"]
+    >
+  >,
+  Expect<Equal<Permutation<never>, []>>
+];
